488d9e93286bec81de02028f0827cc02e35a246b
Walter Moreira <walter@waltermoreira.net>
2026-06-09 22:17:32 -0500

    Load plugins in library

--- Pluma.lean
+++ Pluma.lean
@@ -1,3 +1,5 @@
--- This module serves as the root of the `OEISLt` library.
+-- This module serves as the root of the `Pluma` library.
 -- Import modules here that should be built as part of the library.
 import Pluma.Cli.Basic
+import Pluma.Plugins.Dummy
+import Pluma.Plugins.Eval
